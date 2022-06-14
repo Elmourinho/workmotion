53d47d50c9df77c0b21073f4020c9ab92fa8c3e4
Elmar Alizada <elmar_alizade@hotmail.com>
2022-06-14 18:19:37 +0300

    Minor refactoring

--- src/main/java/com/elmar/workmotion/model/input/EmployeeInput.java
+++ src/main/java/com/elmar/workmotion/model/input/EmployeeInput.java
@@ -1,6 +1,5 @@
 package com.elmar.workmotion.model.input;
 
-import com.elmar.workmotion.model.States;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,5 +8,4 @@ import lombok.Setter;
 public class EmployeeInput {
 	private String name;
 	private Short age;
-	private States state;
 }
